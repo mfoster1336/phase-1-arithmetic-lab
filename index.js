934b5f093ec6925fe414015d030d516a8f3a800e
mikalfoster <mikalfoster36@gmail.com>
2023-02-24 23:41:06 -0600

    done and done

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-const multiply = (num1) => {
-    let num1 = 31
-    return num1 * 2
-}
-const random => Math.random();
-
-const mod = function (num3 % num4){
-    return Math.random()
-}
-const max = Math.max[2, 6, 8, 20]
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2;
+const random = Math.floor(Math.random() * 10) + 1;
+const num3 = 20
+const num4 = 8
+const mod = num3 % num4;
+const max = Math.max(2, 6, 8, 20);
